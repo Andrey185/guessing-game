1baf704041236410687f4decfe885257086badf6
Andrey <rimmon9@gmail.com>
2023-01-26 12:14:48 +0300

    feat: class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min,
+        this.max = max    
     }
-
     guess() {
 
+         this.candidate = Math.round((this.max + this.min)/2);
+         return this.candidate
     }
 
     lower() {
-
+        this.max = this.candidate;
+        this.guess();
     }
 
     greater() {
-
+        this.min = this.candidate;
+        this.guess();
     }
 }
 
